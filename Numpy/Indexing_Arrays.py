dbddffaf71ed84328dcbb0f35bc33238740c0d15
Abdullah-crns <u2023037@giki.edu.pk>
2025-08-05 12:11:47 +0500

    updated

--- Numpy/Indexing_Arrays.py
+++ Numpy/Indexing_Arrays.py
@@ -1,22 +1,22 @@
 import numpy as np
 
-# #Get the first element of the array
-# array = np.array([1,2,3,4,5])
-# print(array[0])
+#Get the first element of the array
+array = np.array([1,2,3,4,5])
+print(array[0])
 
-# #Getthe first and third element of the array and add them
-# array = np.array([1,2,3,4,5])
-# print(array[0]+array[2])
+#Getthe first and third element of the array and add them
+array = np.array([1,2,3,4,5])
+print(array[0]+array[2])
 
-# #Accessing 2D array
-# array = np.array([[1,2,3,4,5],[6,7,8,9,10]])
-# print("2nd element of first row",array[0,1])
+#Accessing 2D array
+array = np.array([[1,2,3,4,5],[6,7,8,9,10]])
+print("2nd element of first row",array[0,1])
 
-# # Accessing 3D array
-# array = np.array([[[1,2,3,4,5],[6,7,8,9,10]],[[1,2,3,4,5],[6,7,8,9,10]]])
-# print("2nd element of first row of first array :",array[0,1,2])
+# Accessing 3D array
+array = np.array([[[1,2,3,4,5],[6,7,8,9,10]],[[1,2,3,4,5],[6,7,8,9,10]]])
+print("2nd element of first row of first array :",array[0,1,2])
 
 
-# # Negative Indexing
-# arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
-# print ("Accessing the last element of the 2nd dimension : ",arr[1,-1])
+# Negative Indexing
+arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+print ("Accessing the last element of the 2nd dimension : ",arr[1,-1])
